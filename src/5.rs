3ef6c57e6de080dfb801283a58629d905fe02cd7
ruewol4xie <ruewol4xie@gmx.com>
2025-03-08 20:58:24 +0000

    Add 5.rs

---
+++ src/5.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut students = HashMap::new();
+    students.insert("John Doe", 12);
+    students.insert("Jane Doe", 10);
+    students.insert("Bob Smith", 8);
+}
